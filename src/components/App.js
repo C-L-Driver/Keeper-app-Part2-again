f38e13a5866a142a1d5b3878663d6e82de1eda01
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-05-20 00:40:26 +0100

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+console.log(notes);
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((singleNote) => (
+        <Note
+          key={singleNote.key}
+          title={singleNote.title}
+          content={singleNote.content}
+        />
+      ))}
       <Footer />
     </div>
   );
